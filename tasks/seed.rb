de1f3886abbfc48b6b70445fd66f22ee08f093a1
Marcos Wolff <wmarcos@gmail.com>
2017-12-15 15:59:46 -0300

    Fix: Agrega datos de País y Provincia (que no tienen ABM) para que se puedan cargar encuestas. Se ejecuta con bundle exec rake seed:basic:dev

--- tasks/seed.rb
+++ tasks/seed.rb
@@ -10,4 +10,16 @@ namespace :seed do
       end
     end
   end
+  # carga país y provincia que no tienen ABM
+  namespace :basic do
+    task :dev do
+      c = Country.create do |country|
+        country.name = (0...20).map { ('a'..'z').to_a[rand(26)] }.join
+      end
+      p = Province.create do |province|
+        province.name = (0...20).map { ('a'..'z').to_a[rand(26)] }.join
+        province.country = c
+      end
+    end
+  end
 end
